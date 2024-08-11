7f760c0373d298c6079119235c6f741bc6e2f934
Kauê Eduardo <148647455+Kaue-Eduardo@users.noreply.github.com>
2024-08-11 18:32:15 -0300

    Add files via upload

--- Mundo_2/Aula13/aula13desafio48_soma_impares_multiplos_de_3.py
+++ Mundo_2/Aula13/aula13desafio48_soma_impares_multiplos_de_3.py
@@ -3,7 +3,7 @@
 soma = 0
 
 # Laço de repetição de 1 a 500
-for i in range(1, 500):
+for i in range(1, 501):
     if i % 2 != 0:
         # Condição, múltiplo de 3
         if i % 3 == 0:
@@ -11,4 +11,4 @@ for i in range(1, 500):
             soma += i
 
 # Imprime o resultado da soma
-print('A soma de todos os números ímpares entre 1 e 500 é: {}'.format(soma))
+print('A soma de todos os números ímpares e multiplos de 3 entre 1 e 500 é: {}'.format(soma))
--- Mundo_2/Aula13/aula13desafio52_numero_primo.py
+++ Mundo_2/Aula13/aula13desafio52_numero_primo.py
@@ -2,12 +2,19 @@
 
 # Entrada do número
 num = int(input('Digite um número inteiro: '))
+# Contador que irá receber quantas vezes o número foi dividido
+contador = 0
 
-if num < 2:
-    print('O número {} não é primo!'.format(num))
-if num == 2:
-    print('O número {} é primo!'.format(num))
-elif num % num == 0 and num % 2 != 0:
-    print('O número {} é primo!'.format(num))
+# Laço que percorre todos os número do 1 ao número digitado
+for i in range(1, num + 1):
+    # Condição que divide o número digitado pelo número da iteração do laço
+    if num % i == 0:
+        # Contador que armazena quantas vezes o número foi divisível
+        contador += 1
+
+# Condição que determina se o número digitado é primo ou não,
+# se for primo ele só pode ser divisível por 1 e por ele mesmo
+if contador == 2:
+    print('O número digitado é primo!')
 else:
-    print('O número {} não é primo!'.format(num))
+    print('O número digitado não é primo!')
