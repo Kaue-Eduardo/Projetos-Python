caa8125120e752b835169e19e02bb0fe3e3155af
Kauê Eduardo <148647455+Kaue-Eduardo@users.noreply.github.com>
2024-08-13 19:00:59 -0300

    Add files via upload

---
+++ Aula14/aula14desafio62_melhoria_progressao_aritmetica.py
@@ -0,0 +1,29 @@
+# O programa irá ler um número, depois sua razão da progressão aritmética e
+# depois mostrar 10 ou mais termos dessa progressão
+
+# Entrada do número e da razão
+num = int(input('Digite um número: '))
+razao = int(input('Agora a sua razão da Progressão Aritmética: '))
+
+# Variáveis de controle
+soma = num
+contador = 0
+mais_termos = 1
+
+# Laço de repetição com 10 repetições
+while contador < 10:
+    # Somando os valores da programação
+    soma += razao
+    print(soma)
+
+    # Contador que altera o valor a cada iteração para dar fim ao laço
+    contador += 1
+
+    # Condição para ter mais termos
+    if contador == 10:
+        mais_termos = int(input('Digite a quantidade de termos a mais ou 0 para finalizar: '))
+        contador = contador - mais_termos
+        # Se o termo digitado for 0 o programa encerra
+        if mais_termos == 0:
+            print('Encerrando!')
+            break
